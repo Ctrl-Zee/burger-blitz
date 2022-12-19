9cadd23ec2018e3ccf39ac25187dbafee69d2e2f
andrew_smith_moser <andrew.smith@moserit.com>
2022-12-19 10:41:34 -0500

    Added tabs and lazy loaded pages

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,13 +4,47 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'browse',
+        loadChildren: () =>
+          import('../browse/browse.module').then((m) => m.BrowsePageModule),
+      },
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('../search/search.module').then((m) => m.SearchPageModule),
+      },
+      {
+        path: 'favorites',
+        loadChildren: () =>
+          import('../favorites/favorites.module').then(
+            (m) => m.FavoritesPageModule
+          ),
+      },
+      {
+        path: 'history',
+        loadChildren: () =>
+          import('../history/history.module').then((m) => m.HistoryPageModule),
+      },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('../user/user.module').then((m) => m.UserPageModule),
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/home/browse',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
