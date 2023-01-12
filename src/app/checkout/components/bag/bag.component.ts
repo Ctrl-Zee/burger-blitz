8387fe2b7b245564b389fc0d2f08d4d68225b49f
Ctrl-Zee <indysmittysmith@gmail.com>
2023-01-12 10:27:24 -0500

    Got modal nav-link working

--- src/app/checkout/components/bag/bag.component.ts
+++ src/app/checkout/components/bag/bag.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { IonNav } from '@ionic/angular';
 import { BagStore } from 'src/app/core/stores/bag.store';
 import { CheckoutComponent } from '../checkout/checkout.component';
 
@@ -9,8 +10,16 @@ import { CheckoutComponent } from '../checkout/checkout.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BagComponent implements OnInit {
-  navComponent = CheckoutComponent;
-  constructor(public bagStore: BagStore) {}
+  checkoutComponent = CheckoutComponent;
+  constructor(public bagStore: BagStore, private nav: IonNav) {}
 
   ngOnInit() {}
+
+  /**
+   *  For some reason I can't figure out router-direction="forward" doesn't work in the template.
+   *  Navigating forwards only works when written programatically.
+   */
+  navigateToCheckout() {
+    this.nav.push(this.checkoutComponent);
+  }
 }
